0a424be2d2412079dc40a9d3e2ccd387454fa9d2
Seito Taka <amanoese@gmail.com>
2019-01-20 05:23:35 +0900

    fix animation bugs

--- pages/index.vue
+++ pages/index.vue
@@ -21,11 +21,9 @@
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid autem culpa eum expedita inventore iusto maxime mollitia, nisi odit quo tempore, tenetur totam? Debitis ducimus eligendi ex fuga nisi voluptatem.</p>
       </div>
 
-      <div class="flower">
-        <img :class="isActive" src="~/assets/images/01.jpg" alt="">
-        <img :class="isActive2" src="~/assets/images/02.jpg" alt="">
-        <img :class="isActive3" src="~/assets/images/03.jpg" alt="">
-        <img :class="isActive4" src="~/assets/images/04.jpg" alt="">
+      <div class="flower" ref="flower">
+        <img src="~/assets/images/01.jpg" alt="">
+        <img :src="activeImageUrl" alt="" class="flower-view">
       </div>
     </div>
   </section>
@@ -33,13 +31,14 @@
 
 <script>
 import Logo from '~/components/Logo.vue'
-
 export default {
   components: {
     Logo
   },
   data(){
     return {
+      imageList:['01.jpg','02.jpg','03.jpg','04.jpg'],
+      imageDataList:[],
       scrollY : 0, //現在のスクロール位置
       offsetTop: null, // お花要素のページ内の縦位置
       offsetHeight: null, // お花要素の高さ
@@ -47,72 +46,66 @@ export default {
       grid: 0 // 咲くために必要な変化する高さの段階割合
     }
   },
+  created(){
+    Promise.all(this.imageList
+    .map(name=>import(`~/assets/images/${name}`)))
+    .then(list=>{
+      this.imageDataList=list.map(x=>x.default)
+    })
+  },
   mounted() {
     // スクロールに反応して Vue.js が動くようにする
     window.addEventListener('scroll', this.handleScroll);
+    window.addEventListener('resize', this.handleScroll);
     // mounted のタイミングではまだ DOM が存在しない
-    this.$nextTick(()=>{
-      // document.querySelector で お花の要素をとってくる
-      const flower = document.querySelector(".flower")
-      this.offsetTop = flower.offsetTop // 実際のお花要素のページ内の縦位置
-      this.offsetHeight = flower.offsetHeight // 実際のお花要素のページ内の高さ
-      this.grid = this.offsetHeight / 6 // 半分の位置から 3段階で切り替えたいから ÷6
-      this.innerHeight = window.innerHeight // 実際の window の高さ
-    })
   },
   computed:{
-    position(){
-      // 変化に必要な変数を常に計算させる
-      if(this.offsetTop === null){
-        return -1
-      }
-      return this.scrollY + this.innerHeight - this.offsetTop - (this.offsetHeight / 2)
+    windowBottom(){
+      return this.scrollY + this.innerHeight
     },
-    isActive(){
-      return {
-        active: this.position < 0
-      }
+    gridHeight(){
+      // 半分の位置から n段階で切り替えたいから
+      return this.flower().offsetHeight / (this.imageDataList.length)
     },
-    isActive2(){
-      return {
-        active: 0 <= this.position && this.position < this.grid
-      }
-    },
-    isActive3(){
-      return {
-        active: this.grid <= this.position && this.position < (this.grid * 2)
-      }
-    },
-    isActive4(){
-      return {
-        active: (this.grid * 2 )<= this.position
-      }
+    activeImageUrl(){
+      let n = Math.floor(this.position() / this.gridHeight)
+      return this.imageDataList[Math.min(this.imageDataList.length-1,Math.max(0,n))] //List外にださない
     }
   },
   methods:{
+    flower(){
+      return this.$refs.flower || {}
+    },
     handleScroll() {
       // 実際のスクロール位置を Vue.js の変数に格納
+      this.innerHeight = window.innerHeight
       this.scrollY = window.scrollY
-    }
+    },
+    position(){
+      // 変化に必要な変数を常に計算させる
+      // 半分の位置から切り替える
+      return this.windowBottom - this.flower().offsetTop - (this.flower().offsetHeight / 2) + this.gridHeight || -1
+    },
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  .container {
-    margin:  0 auto;
-    padding: 50px 10px;
-    max-width: 375px;
-  }
+.container {
+  margin:  0 auto;
+  padding: 50px 10px;
+  max-width: 375px;
+}
 
 .flower {
-  img{
-    display: none;
-  }
-  img.active{
+  position: relative;
+  img {
     width: 100%;
     display: block;
-
+    &.flower-view {
+      position: absolute;
+      top:0;
+    }
   }
 }
 </style>
